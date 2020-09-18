87352a69861c00b7633fb24c7ff77cdf40f082df
Ram Basnet <rambasnet@gmail.com>
2020-09-18 14:08:39 -0400

    added echoServer and my-hacking-network.

---
+++ booksrc/echoServer.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include "hacking.h"
+#include "my-hacking-network.h"
+
+#define PORT 200   // the port users will be connecting to
+
+void handle_connection(int, struct sockaddr_in *); // handle web requests
+
+int main(void) {
+   int sockfd, new_sockfd, yes=1; 
+   struct sockaddr_in host_addr, client_addr;   // my address information
+   socklen_t sin_size;
+
+   printf("Accepting requests on port %d\n", PORT);
+
+   if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+      fatal("in socket");
+
+   if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1)
+      fatal("setting socket option SO_REUSEADDR");
+
+   host_addr.sin_family = AF_INET;      // host byte order
+   host_addr.sin_port = htons(PORT);    // short, network byte order
+   host_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
+   memset(&(host_addr.sin_zero), '\0', 8); // zero the rest of the struct
+
+   if (bind(sockfd, (struct sockaddr *)&host_addr, sizeof(struct sockaddr)) == -1)
+      fatal("binding to socket");
+
+   if (listen(sockfd, 20) == -1)
+      fatal("listening on socket");
+
+   while(1) {   // Accept loop
+      sin_size = sizeof(struct sockaddr_in);
+      new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &sin_size);
+      if(new_sockfd == -1)
+         fatal("accepting connection");
+
+      handle_connection(new_sockfd, &client_addr);
+   }
+   return 0;
+}
+
+/* This function handles the connection on the passed socket from the
+ * passed client address.  The connection is processed as a web request
+ * and this function replies over the connected socket.  Finally, the 
+ * passed socket is closed at the end of the function.
+ */
+void handle_connection(int sockfd, struct sockaddr_in *client_addr_ptr) {
+   unsigned char *ptr, buffer[500];
+   int fd, length;
+
+   length = get_internet_data(sockfd, buffer);
+
+   printf("Got request from %s:%d \"%s\"\n", inet_ntoa(client_addr_ptr->sin_addr), ntohs(client_addr_ptr->sin_port), buffer);
+   echo(sockfd, buffer);
+   shutdown(sockfd, SHUT_RDWR); // close the socket gracefully
+}
