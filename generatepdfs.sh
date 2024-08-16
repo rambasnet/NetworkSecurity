0670e52369a4892b109a6f88ada8ef9b9705af20
Ram Basnet <rambasnet@gmail.com>
2024-08-16 14:06:59 -0600

    update generatepdfs.sh

--- generatepdfs.sh
+++ generatepdfs.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+# Script to convert jupyter notebooks to PDF files. The pdfs are saved in ./pdfs folder
+
+sudo apt update
 sudo apt install -y pandoc texlive texlive-xetex > /dev/null
 
 files=$(find . -name '*.ipynb')
